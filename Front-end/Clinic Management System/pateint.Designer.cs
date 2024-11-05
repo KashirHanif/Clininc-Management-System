22d0ef617ff68979af437737821df93fcb997996
Maleaha Mansoor <maleahawork@gmail.com>
2024-11-05 11:17:34 -0700

    fourth

---
+++ Front-end/Clinic Management System/pateint.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace Clinic_Management_System
+{
+    partial class pateint
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(pateint));
+            this.addpatientTextbox = new System.Windows.Forms.Button();
+            this.updatepatientTextbox = new System.Windows.Forms.Button();
+            this.viewpatientTextbox = new System.Windows.Forms.Button();
+            this.addapointmentTextbox = new System.Windows.Forms.Button();
+            this.cancelappointmentTextbox = new System.Windows.Forms.Button();
+            this.viewappointmentTextbox = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.mediAidNameLabel = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // addpatientTextbox
+            // 
+            this.addpatientTextbox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.addpatientTextbox.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addpatientTextbox.Location = new System.Drawing.Point(51, 223);
+            this.addpatientTextbox.Name = "addpatientTextbox";
+            this.addpatientTextbox.Size = new System.Drawing.Size(174, 46);
+            this.addpatientTextbox.TabIndex = 1;
+            this.addpatientTextbox.Text = "add patient";
+            this.addpatientTextbox.UseVisualStyleBackColor = false;
+            this.addpatientTextbox.Click += new System.EventHandler(this.addpatientTextbox_Click);
+            // 
+            // updatepatientTextbox
+            // 
+            this.updatepatientTextbox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.updatepatientTextbox.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.updatepatientTextbox.Location = new System.Drawing.Point(51, 292);
+            this.updatepatientTextbox.Name = "updatepatientTextbox";
+            this.updatepatientTextbox.Size = new System.Drawing.Size(174, 47);
+            this.updatepatientTextbox.TabIndex = 2;
+            this.updatepatientTextbox.Text = "update patient";
+            this.updatepatientTextbox.UseVisualStyleBackColor = false;
+            // 
+            // viewpatientTextbox
+            // 
+            this.viewpatientTextbox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.viewpatientTextbox.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewpatientTextbox.Location = new System.Drawing.Point(51, 365);
+            this.viewpatientTextbox.Name = "viewpatientTextbox";
+            this.viewpatientTextbox.Size = new System.Drawing.Size(174, 45);
+            this.viewpatientTextbox.TabIndex = 3;
+            this.viewpatientTextbox.Text = "view patients";
+            this.viewpatientTextbox.UseVisualStyleBackColor = false;
+            // 
+            // addapointmentTextbox
+            // 
+            this.addapointmentTextbox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.addapointmentTextbox.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addapointmentTextbox.Location = new System.Drawing.Point(51, 437);
+            this.addapointmentTextbox.Name = "addapointmentTextbox";
+            this.addapointmentTextbox.Size = new System.Drawing.Size(174, 48);
+            this.addapointmentTextbox.TabIndex = 4;
+            this.addapointmentTextbox.Text = "add appointment";
+            this.addapointmentTextbox.UseVisualStyleBackColor = false;
+            // 
+            // cancelappointmentTextbox
+            // 
+            this.cancelappointmentTextbox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.cancelappointmentTextbox.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelappointmentTextbox.Location = new System.Drawing.Point(51, 513);
+            this.cancelappointmentTextbox.Name = "cancelappointmentTextbox";
+            this.cancelappointmentTextbox.Size = new System.Drawing.Size(174, 58);
+            this.cancelappointmentTextbox.TabIndex = 5;
+            this.cancelappointmentTextbox.Text = "cancel appointment";
+            this.cancelappointmentTextbox.UseVisualStyleBackColor = false;
+            // 
+            // viewappointmentTextbox
+            // 
+            this.viewappointmentTextbox.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.viewappointmentTextbox.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewappointmentTextbox.Location = new System.Drawing.Point(51, 594);
+            this.viewappointmentTextbox.Name = "viewappointmentTextbox";
+            this.viewappointmentTextbox.Size = new System.Drawing.Size(174, 45);
+            this.viewappointmentTextbox.TabIndex = 6;
+            this.viewappointmentTextbox.Text = "view appointment";
+            this.viewappointmentTextbox.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(65, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(160, 141);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // mediAidNameLabel
+            // 
+            this.mediAidNameLabel.AutoSize = true;
+            this.mediAidNameLabel.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.mediAidNameLabel.Font = new System.Drawing.Font("Century Gothic", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mediAidNameLabel.ForeColor = System.Drawing.Color.Red;
+            this.mediAidNameLabel.Location = new System.Drawing.Point(661, 58);
+            this.mediAidNameLabel.Name = "mediAidNameLabel";
+            this.mediAidNameLabel.Size = new System.Drawing.Size(215, 56);
+            this.mediAidNameLabel.TabIndex = 8;
+            this.mediAidNameLabel.Text = "MediAid";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(747, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 18);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "cure with care";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(315, 180);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.Size = new System.Drawing.Size(561, 459);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // pateint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.mediAidNameLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.viewappointmentTextbox);
+            this.Controls.Add(this.cancelappointmentTextbox);
+            this.Controls.Add(this.addapointmentTextbox);
+            this.Controls.Add(this.viewpatientTextbox);
+            this.Controls.Add(this.updatepatientTextbox);
+            this.Controls.Add(this.addpatientTextbox);
+            this.Name = "pateint";
+            this.Size = new System.Drawing.Size(935, 688);
+            this.Load += new System.EventHandler(this.pateint_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button addpatientTextbox;
+        private System.Windows.Forms.Button updatepatientTextbox;
+        private System.Windows.Forms.Button viewpatientTextbox;
+        private System.Windows.Forms.Button addapointmentTextbox;
+        private System.Windows.Forms.Button cancelappointmentTextbox;
+        private System.Windows.Forms.Button viewappointmentTextbox;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label mediAidNameLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ Front-end/Clinic Management System/pateint.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clinic_Management_System
+{
+    public partial class pateint : UserControl
+    {
+        public pateint()
+        {
+            InitializeComponent();
+        }
+
+        private void pateint_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addpatientTextbox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
