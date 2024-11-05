3a9f370fe2375bf1833989c717299f035a7df835
Maleaha Mansoor <maleahawork@gmail.com>
2024-11-04 23:57:38 -0700

    second commit

--- Front-end/Clinic Management System/Form1.cs
+++ Front-end/Clinic Management System/Form1.cs
@@ -16,5 +16,10 @@ namespace Clinic_Management_System
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
