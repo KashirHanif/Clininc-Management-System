6dda7c620229d36e9b9ec66cff5d31b43b60c6d1
Maleaha Mansoor <maleahawork@gmail.com>
2024-11-25 11:17:00 -0700

    added more styling

--- Front-end/Clinic Management System/menu.Designer.cs
+++ Front-end/Clinic Management System/menu.Designer.cs
@@ -28,22 +28,24 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(menu));
             this.btnPatient = new System.Windows.Forms.Button();
             this.btnDoctors = new System.Windows.Forms.Button();
             this.btnInventory = new System.Windows.Forms.Button();
-            this.btnReports = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // btnPatient
             // 
             this.btnPatient.BackColor = System.Drawing.Color.Maroon;
-            this.btnPatient.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPatient.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnPatient.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnPatient.Location = new System.Drawing.Point(332, 246);
+            this.btnPatient.Location = new System.Drawing.Point(712, 420);
             this.btnPatient.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnPatient.Name = "btnPatient";
-            this.btnPatient.Size = new System.Drawing.Size(389, 89);
+            this.btnPatient.Size = new System.Drawing.Size(560, 125);
             this.btnPatient.TabIndex = 0;
             this.btnPatient.Text = "PATIENT";
             this.btnPatient.UseVisualStyleBackColor = false;
@@ -51,65 +53,68 @@
             // 
             // btnDoctors
             // 
-            this.btnDoctors.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.btnDoctors.ForeColor = System.Drawing.Color.DarkRed;
-            this.btnDoctors.Location = new System.Drawing.Point(332, 372);
+            this.btnDoctors.BackColor = System.Drawing.Color.Maroon;
+            this.btnDoctors.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDoctors.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnDoctors.Location = new System.Drawing.Point(712, 603);
             this.btnDoctors.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnDoctors.Name = "btnDoctors";
-            this.btnDoctors.Size = new System.Drawing.Size(389, 95);
+            this.btnDoctors.Size = new System.Drawing.Size(560, 125);
             this.btnDoctors.TabIndex = 1;
             this.btnDoctors.Text = "DOCTORS";
             this.btnDoctors.UseVisualStyleBackColor = false;
             // 
             // btnInventory
             // 
-            this.btnInventory.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.btnInventory.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnInventory.Location = new System.Drawing.Point(332, 498);
+            this.btnInventory.BackColor = System.Drawing.Color.Maroon;
+            this.btnInventory.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInventory.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnInventory.Location = new System.Drawing.Point(712, 775);
             this.btnInventory.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnInventory.Name = "btnInventory";
-            this.btnInventory.Size = new System.Drawing.Size(389, 84);
+            this.btnInventory.Size = new System.Drawing.Size(560, 126);
             this.btnInventory.TabIndex = 2;
             this.btnInventory.Text = "INVENTORY";
             this.btnInventory.UseVisualStyleBackColor = false;
             // 
-            // btnReports
-            // 
-            this.btnReports.BackColor = System.Drawing.SystemColors.ControlDark;
-            this.btnReports.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnReports.Location = new System.Drawing.Point(332, 613);
-            this.btnReports.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.btnReports.Name = "btnReports";
-            this.btnReports.Size = new System.Drawing.Size(389, 91);
-            this.btnReports.TabIndex = 3;
-            this.btnReports.Text = "REPORTS ";
-            this.btnReports.UseVisualStyleBackColor = false;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(460, 133);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(909, 306);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(108, 36);
+            this.label1.Size = new System.Drawing.Size(165, 54);
             this.label1.TabIndex = 4;
             this.label1.Text = "MENU";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(645, 74);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(791, 180);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
             // 
             // menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnReports);
             this.Controls.Add(this.btnInventory);
             this.Controls.Add(this.btnDoctors);
             this.Controls.Add(this.btnPatient);
             this.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximumSize = new System.Drawing.Size(2000, 1100);
+            this.MinimumSize = new System.Drawing.Size(2000, 1100);
             this.Name = "menu";
-            this.Size = new System.Drawing.Size(1069, 796);
+            this.Size = new System.Drawing.Size(2000, 1100);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -120,7 +125,7 @@
         private System.Windows.Forms.Button btnPatient;
         private System.Windows.Forms.Button btnDoctors;
         private System.Windows.Forms.Button btnInventory;
-        private System.Windows.Forms.Button btnReports;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- Front-end/Clinic Management System/pateint.Designer.cs
+++ Front-end/Clinic Management System/pateint.Designer.cs
@@ -37,6 +37,7 @@ namespace Clinic_Management_System
             this.viewAppointmentsButton = new System.Windows.Forms.Button();
             this.patientGridView = new System.Windows.Forms.DataGridView();
             this.mediAidLogo = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.patientGridView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.mediAidLogo)).BeginInit();
             this.SuspendLayout();
@@ -48,7 +49,7 @@ namespace Clinic_Management_System
             this.addPatientButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.addPatientButton.Location = new System.Drawing.Point(50, 250);
             this.addPatientButton.Name = "addPatientButton";
-            this.addPatientButton.Size = new System.Drawing.Size(255, 68);
+            this.addPatientButton.Size = new System.Drawing.Size(255, 65);
             this.addPatientButton.TabIndex = 0;
             this.addPatientButton.Text = "Add Patient";
             this.addPatientButton.UseVisualStyleBackColor = false;
@@ -59,7 +60,7 @@ namespace Clinic_Management_System
             this.updatePatientButton.BackColor = System.Drawing.Color.Maroon;
             this.updatePatientButton.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
             this.updatePatientButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.updatePatientButton.Location = new System.Drawing.Point(50, 335);
+            this.updatePatientButton.Location = new System.Drawing.Point(50, 332);
             this.updatePatientButton.Name = "updatePatientButton";
             this.updatePatientButton.Size = new System.Drawing.Size(255, 65);
             this.updatePatientButton.TabIndex = 1;
@@ -72,7 +73,7 @@ namespace Clinic_Management_System
             this.viewPatientButton.BackColor = System.Drawing.Color.Maroon;
             this.viewPatientButton.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
             this.viewPatientButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.viewPatientButton.Location = new System.Drawing.Point(50, 419);
+            this.viewPatientButton.Location = new System.Drawing.Point(50, 412);
             this.viewPatientButton.Name = "viewPatientButton";
             this.viewPatientButton.Size = new System.Drawing.Size(255, 65);
             this.viewPatientButton.TabIndex = 2;
@@ -85,7 +86,7 @@ namespace Clinic_Management_System
             this.addAppointmentButton.BackColor = System.Drawing.Color.Maroon;
             this.addAppointmentButton.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
             this.addAppointmentButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.addAppointmentButton.Location = new System.Drawing.Point(50, 505);
+            this.addAppointmentButton.Location = new System.Drawing.Point(50, 496);
             this.addAppointmentButton.Name = "addAppointmentButton";
             this.addAppointmentButton.Size = new System.Drawing.Size(255, 68);
             this.addAppointmentButton.TabIndex = 3;
@@ -98,7 +99,7 @@ namespace Clinic_Management_System
             this.cancelAppointmentButton.BackColor = System.Drawing.Color.Maroon;
             this.cancelAppointmentButton.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
             this.cancelAppointmentButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.cancelAppointmentButton.Location = new System.Drawing.Point(50, 589);
+            this.cancelAppointmentButton.Location = new System.Drawing.Point(50, 581);
             this.cancelAppointmentButton.Name = "cancelAppointmentButton";
             this.cancelAppointmentButton.Size = new System.Drawing.Size(255, 64);
             this.cancelAppointmentButton.TabIndex = 4;
@@ -111,7 +112,7 @@ namespace Clinic_Management_System
             this.viewAppointmentsButton.BackColor = System.Drawing.Color.Maroon;
             this.viewAppointmentsButton.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold);
             this.viewAppointmentsButton.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.viewAppointmentsButton.Location = new System.Drawing.Point(50, 676);
+            this.viewAppointmentsButton.Location = new System.Drawing.Point(50, 662);
             this.viewAppointmentsButton.Name = "viewAppointmentsButton";
             this.viewAppointmentsButton.Size = new System.Drawing.Size(255, 60);
             this.viewAppointmentsButton.TabIndex = 5;
@@ -133,11 +134,11 @@ namespace Clinic_Management_System
             dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
             this.patientGridView.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
-            this.patientGridView.ColumnHeadersHeight = 29;
+            this.patientGridView.ColumnHeadersHeight = 32;
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
             dataGridViewCellStyle2.BackColor = System.Drawing.Color.White;
             dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
@@ -149,6 +150,10 @@ namespace Clinic_Management_System
             this.patientGridView.Size = new System.Drawing.Size(1089, 494);
             this.patientGridView.TabIndex = 8;
             this.patientGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.patientGridView_CellContentClick);
+            this.patientGridView.DefaultCellStyle.ForeColor = Color.Black;
+            this.patientGridView.AlternatingRowsDefaultCellStyle.ForeColor = Color.Black;
+            this.patientGridView.RowsDefaultCellStyle.ForeColor = Color.Black;
+            this.patientGridView.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
             // 
             // mediAidLogo
             // 
@@ -159,9 +164,22 @@ namespace Clinic_Management_System
             this.mediAidLogo.TabIndex = 9;
             this.mediAidLogo.TabStop = false;
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkBlue;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(50, 743);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(59, 32);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "back";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
             // PatientUserControl
             // 
             this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.mediAidLogo);
             this.Controls.Add(this.addPatientButton);
             this.Controls.Add(this.updatePatientButton);
@@ -182,5 +200,6 @@ namespace Clinic_Management_System
         }
 
         private System.Windows.Forms.PictureBox mediAidLogo;
+        private Button button1;
     }
 }
